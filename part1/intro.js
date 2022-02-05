21f149c22f344b25ee7dae2bf3b89beace817a70
ImranRH321 <imranrh247@gmail.com>
2022-02-05 20:56:52 +0600

    first commit

---
+++ part1/intro.js
@@ -0,0 +1,142 @@
+// 1 Introduction to JavacripSt, Run JavaScript in VSCode ?
+// JavaScript Output
+
+// document.getElementById('demo').innerHTML= 'Javascript selector id demo';
+// document.write('Outpot browser');
+// window.alert('window alert outpot');
+// console.log(10000000); 
+
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+
+// 2 What is variable, five things you need to declare a variable ?
+// ans: Variables are containers for storing data (storing data values) 
+// var x = 5 ;
+// 4 Ways to Declare a JavaScript Variable:
+// Using var
+// Using let
+// Using const
+// Using nothing
+
+
+// 3 Variable type, Numeric, String, Boolean ? 
+// var number = 1010 ;
+// var student = 'imran';
+// var areYourStudent = false ;
+// console.log(typeof name);
+
+
+// 4.Variable aminame nng convention ?
+// link:   https://www.w3schools.com/js/js_reserved.asp
+
+
+
+// 5 Simple Mathematical operations in JavaScript
+/*  
+JavaScript Arithmetic Operators 
+ +	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Division Remainder)
+++	Increment
+-- 	Decrement
+*/
+
+/* JavaScript Assignment Operators	
+=	x = y		
+-=	x -= y	
+*=	x *= y	
+/=	x /= y	
+%=	x %= y	
+**=	x **= y	 */
+
+/* 
+JavaScript Comparison Operators
+Operator	Description
+==	equal to
+===	equal value and equal type
+!=	not equa
+!==	not equal value or not equal typel
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+?	ternary operator  */
+
+/* JavaScript Logical Operators
+Operator	Description
+&&	logical and
+||	logical or
+!	logical not */
+
+
+// 6. (advanced) Mathematical operation shorthand
+/*
+ var pants = 400 ;
+pants += 1 ;
+pants *= 2 ;
+pants /= 2 ;
+pants %= 2 ;
+pants -= 10 ;
+console.log(pants);
+ */
+
+
+// 7 (advanced) Integer float parseInt parseFloat type conversion 
+
+// var x = '30';    //Integer
+// var y = 30.30333;     // float parseInt
+// console.log(parseInt(y));
+// console.log(parseFloat(y));
+// console.log(parseFloat(y.toFixed(3)));
+// console.log(x);
+// console.log(parseFloat(x.toFixed(1)));
+
+
+// ---------------         Rx --------------------
+
+// Operators
+// Arithmetic operators ( + , - , * , / , -- , ++ , % , **)
+// Assignment Operators ( = , += , -= , *= , /= )
+// Logical Operators ( && , || , !)
+// Comparison  operators (== , === , > , <  => , <= , != , !==)
+
+
+
+// Data Type :
+// 1. Primetime Data Type 
+//     a. Number 
+//     b. String
+//     c. Booleans
+//     d. undifiend
+//     e. null
+//     f. NaN
+
+// // 2. Object 
+//     a. Array 
+//     b. Object
+//     c. Function 
+
+
+// ---------------------- 
+// var x = Number(22)
+// var z = String('hii miss kakiba')
+
+//    console.log(x);
+//    console.log(z);
+ 
+
+
+
+
+
+
+
+
+
+
